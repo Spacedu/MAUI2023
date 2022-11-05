a4e0f100b1b19b245ac7fc8d6f394d91453f656e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-05 14:30:00 -0300

    S03E14 - Projeto Finalizado!

--- ProjetosMAUI/AppNumeroDaSorte/MainPage.xaml.cs
+++ ProjetosMAUI/AppNumeroDaSorte/MainPage.xaml.cs
@@ -11,5 +11,27 @@ public partial class MainPage : ContentPage
 	{
 		NameApp.IsVisible = false;
 		ContainerLuckNumbers.IsVisible = true;
+
+		var set = GenerateLuckNumbers();
+
+		LuckNumber01.Text = set.ElementAt(0).ToString("D2");
+		LuckNumber02.Text = set.ElementAt(1).ToString("D2");
+		LuckNumber03.Text = set.ElementAt(2).ToString("D2");
+		LuckNumber04.Text = set.ElementAt(3).ToString("D2");
+		LuckNumber05.Text = set.ElementAt(4).ToString("D2");
+		LuckNumber06.Text = set.ElementAt(5).ToString("D2");
+    }
+
+	private SortedSet<int> GenerateLuckNumbers()
+	{
+		var set = new SortedSet<int>();
+		while(set.Count < 6){ 
+			var random = new Random();
+			var luckNumber = random.Next(1, 60);
+
+            set.Add(luckNumber);
+        }
+
+		return set;
     }
 }

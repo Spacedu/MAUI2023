d4d086c6c8533829f3a2a1c807c0dd1271e8d553
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-09 12:11:14 -0300

    S04E03 - Explicado a ContentPage e o XAML

---
+++ ProjetosMAUI/AppContentPage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppContentPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ProjetosMAUI/AppContentPage/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppContentPage;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}

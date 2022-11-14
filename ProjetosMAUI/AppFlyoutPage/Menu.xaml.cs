b3fe6a7ef52da8d1293e32d67996cf47c825fe5e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-14 09:30:12 -0300

    S04E07 - FlyoutPage - Parte 2
    Navegando através do menu.

--- ProjetosMAUI/AppFlyoutPage/Menu.xaml.cs
+++ ProjetosMAUI/AppFlyoutPage/Menu.xaml.cs
@@ -6,4 +6,19 @@ public partial class Menu : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnButtonClickedPage1(object sender, EventArgs e)
+    {
+        ((FlyoutPage)App.Current.MainPage).Detail = new Page1();
+    }
+
+    private void OnButtonClickedPage2(object sender, EventArgs e)
+    {
+        ((FlyoutPage)App.Current.MainPage).Detail = new Page2();
+    }
+
+    private void OnButtonClickedPage3(object sender, EventArgs e)
+    {
+        ((FlyoutPage)App.Current.MainPage).Detail = new Page3();
+    }
 }

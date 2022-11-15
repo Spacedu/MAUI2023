d75211c70e5bca8ab7927aff9e8bb3742f9fedca
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-15 14:32:09 -0300

    S05E04 - Programando o menu Lateral.

--- ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
@@ -1,3 +1,5 @@
+using AppMAUIGallery.Repositories;
+
 namespace AppMAUIGallery.Views;
 
 public partial class Menu : ContentPage
@@ -5,5 +7,25 @@ public partial class Menu : ContentPage
 	public Menu()
 	{
 		InitializeComponent();
-	}
+
+		var categories = new CategoryRepository().GetCategories();
+		foreach (var category in categories)
+		{
+            var lblCategory = new Label();
+            lblCategory.Text = category.Name;
+
+			MenuContainer.Children.Add(lblCategory);
+			foreach (var component in category.Components)
+			{
+				var lblComponentTitle = new Label();
+				lblComponentTitle.Text = component.Title;
+
+				var lblComponentDescription = new Label();
+				lblComponentDescription.Text = component.Description;
+
+				MenuContainer.Children.Add(lblComponentTitle);
+				MenuContainer.Children.Add(lblComponentDescription);
+			}
+        }
+    }
 }

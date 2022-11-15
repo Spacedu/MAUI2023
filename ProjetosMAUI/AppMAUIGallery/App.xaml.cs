03c10f6bf14694894ea30c97bec4849cbba5b5bc
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-15 13:26:30 -0300

    S05E01 - App Gallery - Criando e configurando Icone e Splash

---
+++ ProjetosMAUI/AppMAUIGallery/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMAUIGallery
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

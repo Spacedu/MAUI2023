155df507feac1d8326ed5fc1bce2e37d73c9a9a3
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-11-15 16:57:18 -0300

    S05E06 - Abrindo página do Component

--- ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
@@ -18,18 +18,34 @@ public partial class Menu : ContentPage
 			MenuContainer.Children.Add(lblCategory);
 			foreach (var component in category.Components)
 			{
+				var tap = new TapGestureRecognizer();
+				tap.CommandParameter = component.Page;
+				tap.Tapped += OnTapComponent;
+
 				var lblComponentTitle = new Label();
 				lblComponentTitle.Text = component.Title;
 				lblComponentTitle.FontFamily = "OpenSansSemibold";
 				lblComponentTitle.Margin = new Thickness(20, 20, 0, 0);
+				lblComponentTitle.GestureRecognizers.Add(tap);
 
-                var lblComponentDescription = new Label();
+				var lblComponentDescription = new Label();
 				lblComponentDescription.Text = component.Description;
 				lblComponentDescription.Margin = new Thickness(20, 0, 0, 0);
+				lblComponentDescription.GestureRecognizers.Add(tap);
 
 				MenuContainer.Children.Add(lblComponentTitle);
 				MenuContainer.Children.Add(lblComponentDescription);
 			}
         }
+    }
+
+	private void OnTapComponent(object sender, EventArgs e)
+	{
+		var label = (Label)sender;
+		var tap = (TapGestureRecognizer)label.GestureRecognizers[0];
+		var page = (Page)tap.CommandParameter;
+
+		((FlyoutPage)App.Current.MainPage).Detail = page;
+		((FlyoutPage)App.Current.MainPage).IsPresented = false;
     }
 }

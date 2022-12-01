524f938a43ac08f93251df13639e5a0c3cb3a047
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-01 09:36:38 -0300

    S06E13 - Image - Parte 2

--- ProjetosMAUI/AppMAUIGallery/Views/Components/Mains/ImagePage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Components/Mains/ImagePage.xaml.cs
@@ -5,5 +5,12 @@ public partial class ImagePage : ContentPage
 	public ImagePage()
 	{
 		InitializeComponent();
-	}
+        /*
+		Imagem01.Source pode ter 4 valores:
+		- ImageSource.FromFile
+		- ImageSource.FromResource
+		- ImageSource.FromStream
+		- ImageSource.FromUri
+		*/
+    }
 }

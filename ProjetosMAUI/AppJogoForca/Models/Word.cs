18c88bb3aaa8eaedc6beef38bc450430deba86c5
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-02 10:30:02 -0300

    S07E05 - Models e Repositories

---
+++ ProjetosMAUI/AppJogoForca/Models/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Models
+{
+    public class Word
+    {
+        public Word(String tips, String text) { 
+            this.Tips = tips;
+            this.Text = text;
+        }
+        public String Tips { get; set; } = string.Empty;
+        public String Text { get; set; } = string.Empty;
+    }
+}
---
+++ ProjetosMAUI/AppJogoForca/Repositories/WordRepositories.cs
@@ -0,0 +1,28 @@
+﻿using AppJogoForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Repositories
+{
+    public class WordRepositories
+    {
+        private List<Word> _words;
+        public WordRepositories() { 
+            _words = new List<Word>();
+            _words.Add(new Word("Nome", "Maria"));
+            _words.Add(new Word("Vegetal", "Cenoura"));
+            _words.Add(new Word("Fruta", "Abacate"));
+            _words.Add(new Word("Tempero", "Nordestino"));
+            _words.Add(new Word("Tempero", "Baiano"));
+        }
+        public Word GetRandomWord()
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, _words.Count);
+            return _words[number];
+        }
+    }
+}

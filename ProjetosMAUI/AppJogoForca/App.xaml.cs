1b019330f3d132ed6783c88214ab1325d3950098
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-02 09:58:17 -0300

    S07E02 - Projeto criado
    Configurado Nome, Icone e Splash.

---
+++ ProjetosMAUI/AppJogoForca/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppJogoForca
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProjetosMAUI/AppJogoForca/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppJogoForca
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

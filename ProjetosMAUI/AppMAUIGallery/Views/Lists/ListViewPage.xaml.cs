0bc09fcaded16378b2cc7abffb4209252f5e3fe1
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-01-30 11:59:33 -0300

    S11E09 - ListView - Refresh, Separator - Parte 5

--- ProjetosMAUI/AppMAUIGallery/Views/Lists/ListViewPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Lists/ListViewPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class ListViewPage : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        ListViewControl.ItemsSource = MovieList.GetList();
+        ListViewControl.ItemsSource = MovieList.GetList().Take(2);
     }
 
     private void ListViewControl_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -19,5 +19,14 @@ public partial class ListViewPage : ContentPage
         var movie = (Movie)e.SelectedItem;
 
         App.Current.MainPage.DisplayAlert("Filme selecionado!", $"O filme selecionado é: {movie.Name}", "OK");
+    }
+
+    private async void ListViewControl_Refreshing(object sender, EventArgs e)
+    {
+        ListViewControl.IsRefreshing = true;
+        await Task.Delay(3000);
+        ListViewControl.ItemsSource = MovieList.GetList();
+
+        ListViewControl.IsRefreshing = false;
     }
 }

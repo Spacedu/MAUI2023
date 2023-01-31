60ecb8cc6e43a72e779dce8853030154d92d39fb
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-01-30 22:07:55 -0300

    S11E13 - CollectionView - usando RefreshView - Parte 2

--- ProjetosMAUI/AppMAUIGallery/Views/Lists/CollectionViewPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Lists/CollectionViewPage.xaml.cs
@@ -10,4 +10,14 @@ public partial class CollectionViewPage : ContentPage
 
 		CollectionViewControl.ItemsSource = MovieList.GetList().Take(5);
 	}
+
+    private async void RefreshView_Refreshing(object sender, EventArgs e)
+	{
+		((RefreshView)sender).IsRefreshing = true;
+
+		await Task.Delay(3000);
+		CollectionViewControl.ItemsSource = MovieList.GetList();
+
+        ((RefreshView)sender).IsRefreshing = false;
+    }
 }

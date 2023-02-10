22f77d8df490f113438a5e264f65a61aa97a1b01
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-10 14:16:55 -0300

    S11E28 - DataTemplateSelector - Parte 3

---
+++ ProjetosMAUI/AppMAUIGallery/Views/Lists/Utils/MovieTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using AppMAUIGallery.Views.Lists.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Views.Lists.Utils
+{
+    public class MovieTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TemplateDefault { get; set; }
+        public DataTemplate TemplateLongMovie { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Movie movie = (Movie)item;
+            
+            return (movie.Duration.Hours > 1) ? TemplateLongMovie : TemplateDefault;
+        }
+    }
+}

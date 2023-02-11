5706e95140299d73f414eb28bbe18a0c2a05e71b
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-11 16:26:23 -0300

    S12E05 - Menu - CollectionView - Carregando dados

--- ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Menu.xaml.cs
@@ -4,39 +4,15 @@ namespace AppMAUIGallery.Views;
 
 public partial class Menu : ContentPage
 {
-	public Menu()
+	private IGroupComponentRepository _repository;
+    public Menu()
 	{
 		InitializeComponent();
 
-		var categories = new CategoryRepository().GetCategories();
-		foreach (var category in categories)
-		{
-            var lblCategory = new Label();
-            lblCategory.Text = category.Name;
-			lblCategory.FontFamily = "OpenSansSemibold";
+		//TODO - DI - Dependecy Injection
+		_repository = new GroupComponentRepository();
 
-			MenuContainer.Children.Add(lblCategory);
-			foreach (var component in category.Components)
-			{
-				var tap = new TapGestureRecognizer();
-				tap.CommandParameter = component.Page;
-				tap.Tapped += OnTapComponent;
-
-				var lblComponentTitle = new Label();
-				lblComponentTitle.Text = component.Title;
-				lblComponentTitle.FontFamily = "OpenSansSemibold";
-				lblComponentTitle.Margin = new Thickness(20, 20, 0, 0);
-				lblComponentTitle.GestureRecognizers.Add(tap);
-
-				var lblComponentDescription = new Label();
-				lblComponentDescription.Text = component.Description;
-				lblComponentDescription.Margin = new Thickness(20, 0, 0, 0);
-				lblComponentDescription.GestureRecognizers.Add(tap);
-
-				MenuContainer.Children.Add(lblComponentTitle);
-				MenuContainer.Children.Add(lblComponentDescription);
-			}
-        }
+		MenuCollection.ItemsSource = _repository.GetGroupComponents();
     }
 
 	private void OnTapComponent(object sender, EventArgs e)

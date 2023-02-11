6b5606743b35564c44119288c6cb9384b262a87f
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-11 17:18:13 -0300

    S12E08 - MainPage - Carregando os dados

--- ProjetosMAUI/AppMAUIGallery/Views/MainPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/MainPage.xaml.cs
@@ -1,9 +1,25 @@
+using AppMAUIGallery.Repositories;
+
 namespace AppMAUIGallery.Views;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    private IGroupComponentRepository _repository;
+
+    public MainPage()
 	{
 		InitializeComponent();
+
+        _repository = new GroupComponentRepository();
+
+        ComponentCollection.ItemsSource = _repository.GetComponents();
 	}
+
+    private void OnTapComponent(object sender, TappedEventArgs e)
+    {
+        var page = (Type)e.Parameter;
+
+        ((FlyoutPage)App.Current.MainPage).Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+        ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
 }

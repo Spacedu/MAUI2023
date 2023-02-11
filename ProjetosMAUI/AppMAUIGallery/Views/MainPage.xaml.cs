0a50531940798aaae65e10bc1be07e4470813e9b
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-11 19:08:45 -0300

    S12E09 - MainPage - Pesquisa - Parte 1

--- ProjetosMAUI/AppMAUIGallery/Views/MainPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/MainPage.xaml.cs
@@ -1,3 +1,4 @@
+using AppMAUIGallery.Models;
 using AppMAUIGallery.Repositories;
 
 namespace AppMAUIGallery.Views;
@@ -5,14 +6,17 @@ namespace AppMAUIGallery.Views;
 public partial class MainPage : ContentPage
 {
     private IGroupComponentRepository _repository;
+    private List<Component> _fullList;
 
     public MainPage()
 	{
 		InitializeComponent();
 
         _repository = new GroupComponentRepository();
+        _fullList = _repository.GetComponents();
 
-        ComponentCollection.ItemsSource = _repository.GetComponents();
+
+        ComponentCollection.ItemsSource = _fullList;
 	}
 
     private void OnTapComponent(object sender, TappedEventArgs e)
@@ -21,5 +25,18 @@ public partial class MainPage : ContentPage
 
         ((FlyoutPage)App.Current.MainPage).Detail = new NavigationPage((Page)Activator.CreateInstance(page));
         ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
+
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var word = e.NewTextValue;
+
+        _fullList.RemoveAt(0);
+
+        /*
+        ComponentCollection.ItemsSource = _repository.GetComponents()
+            .Where(a=>a.Title.ToLower().Contains(word.ToLower()))
+            .ToList();
+        */
     }
 }

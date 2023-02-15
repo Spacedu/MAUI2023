04c95e2a733e0abdd377cdf3a28a54ca14c9aa6a
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-15 12:53:50 -0300

    S13E07 - Lógica nos botões.

--- ProjetosMAUI/AppTask/Views/AddEditTaskPage.xaml.cs
+++ ProjetosMAUI/AppTask/Views/AddEditTaskPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class AddEditTaskPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void CloseModal(object sender, EventArgs e)
+    {
+		Navigation.PopModalAsync();
+    }
+
+    private void SaveData(object sender, EventArgs e)
+    {
+        Navigation.PopModalAsync();
+    }
+
+    private async void AddStep(object sender, EventArgs e)
+    {
+        var stepName = await DisplayPromptAsync("Etapa(subtarefa)", "Digite o nome da etapa(subtarefa):", "Adicionar", "Cancelar");
+    }
 }

dcc4cb52c237ccc1fc974c5e0dc595fb167a73ab
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-15 12:46:39 -0300

    S13E06 - AddEditTaskPage

--- ProjetosMAUI/AppTask/Views/StartPage.xaml.cs
+++ ProjetosMAUI/AppTask/Views/StartPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class StartPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushModalAsync(new AddEditTaskPage());
+    }
 }

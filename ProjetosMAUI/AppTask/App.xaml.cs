6933dadc2af5bcdf01170de61eb4dce86fb7e3c9
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-15 10:46:34 -0300

    S13E03 - Criar projeto e Configurar Icone e SplashScreen

---
+++ ProjetosMAUI/AppTask/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}

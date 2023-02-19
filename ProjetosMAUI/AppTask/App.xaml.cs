987c60b0be12d2f02bf206101691e9e9cbedbbc2
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-19 10:46:14 -0300

    S13E08 - Custom Handler - No Border

--- ProjetosMAUI/AppTask/App.xaml.cs
+++ ProjetosMAUI/AppTask/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using AppTask.Views;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Platform;
 
 namespace AppTask
 {
@@ -6,9 +8,26 @@ namespace AppTask
     {
         public App()
         {
+            CustomHandler();
+
             InitializeComponent();
 
             MainPage = new NavigationPage(new StartPage());
+        }
+        private void CustomHandler()
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) => {
+#if ANDROID
+                //ANDROID
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#elif IOS || MACCATALYST
+                //iOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //WINDOWS - Não funciona 100%
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
         }
     }
 }

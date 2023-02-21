69231186a26b7ae5d66d904d5429eee571e082aa
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-21 15:17:41 -0300

    S13E13 - TaskModel e SubTaskModel

---
+++ ProjetosMAUI/AppTask.Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTask.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public DateTime PrevisionDate { get; set;}
+        public bool IsCompleted { get; set; }
+        public List<SubTaskModel> SubTasks { get; set; } = new List<SubTaskModel>();
+    }
+}

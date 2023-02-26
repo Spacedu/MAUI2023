b958883b1c46a0ac27563ff402bbb3b7c38256eb
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-25 22:29:58 -0300

    S13E15 - Configurar DbContext - Parte 1

---
+++ ProjetosMAUI/AppTask.Database/AppTaskContext.cs
@@ -0,0 +1,14 @@
+﻿using AppTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTask.Database
+{
+    public class AppTaskContext : DbContext
+    {
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<SubTaskModel> SubTasks { get; set; }
+    }
+}

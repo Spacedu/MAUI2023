542418e3f38dde238a8ca0a9b27ee64599ebd567
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2023-02-25 22:46:58 -0300

    S13E16 - Configurar DbContext - Parte 2.trec

--- ProjetosMAUI/AppTask.Database/AppTaskContext.cs
+++ ProjetosMAUI/AppTask.Database/AppTaskContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace AppTask.Database
@@ -10,5 +11,14 @@ namespace AppTask.Database
     {
         public DbSet<TaskModel> Tasks { get; set; }
         public DbSet<SubTaskModel> SubTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "apptask.db");
+
+            optionsBuilder.UseSqlite($"Filename={databasePath}");
+        }
     }
 }

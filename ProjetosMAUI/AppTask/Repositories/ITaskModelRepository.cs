6a2ce4cade16b1f3ce17748f688362c1f3d86e6b
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-13 11:29:42 -0300

    S13E20 - Repository - Criando a Interface e Classe - Parte 1

---
+++ ProjetosMAUI/AppTask/Repositories/ITaskModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using AppTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTask.Repositories
+{
+    public interface ITaskModelRepository
+    {
+        IList<TaskModel> GetAll();
+        TaskModel GetById(int id);
+        void Add(TaskModel task);
+        void Update(TaskModel task);
+        void Delete(TaskModel task);
+    }
+}
---
+++ ProjetosMAUI/AppTask/Repositories/TaskModelRepository.cs
@@ -0,0 +1,33 @@
+﻿using AppTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTask.Repositories
+{
+    public class TaskModelRepository : ITaskModelRepository
+    {
+        public IList<TaskModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public TaskModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(TaskModel task)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(TaskModel task)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(TaskModel task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

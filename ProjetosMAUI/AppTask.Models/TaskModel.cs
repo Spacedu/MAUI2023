020589256ca30bb953261fbb6a7ddfd853a46fc8
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-18 16:13:58 -0300

    S13E27 - StartPage - Tarefa Concluída - Parte 3 - INotifyPropertyChanged

--- ProjetosMAUI/AppTask.Models/TaskModel.cs
+++ ProjetosMAUI/AppTask.Models/TaskModel.cs
@@ -1,15 +1,33 @@
-﻿namespace AppTask.Models
+﻿using System.ComponentModel;
+
+namespace AppTask.Models
 {
-    public class TaskModel
+    public class TaskModel : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
         public string Description { get; set; } = String.Empty;
         public DateTime PrevisionDate { get; set;}
-        public bool IsCompleted { get; set; }
+
+
+        private bool _isCompleted;
+        public bool IsCompleted {
+            get { return _isCompleted; }
+            set {
+                _isCompleted = value;
+                OnPropertyChanged(nameof(IsCompleted));
+            }
+        }
 
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
         public List<SubTaskModel> SubTasks { get; set; } = new List<SubTaskModel>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
     }
 }

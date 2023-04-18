8dae10af0d360ea22ff411d5af252cd8dc3b9782
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-18 15:26:07 -0300

    S13E26 - StartPage - Tarefa Concluída - Pa

---
+++ ProjetosMAUI/AppTask/Libraries/Converters/BoolToTextDecorationStrikeThroughConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTask.Libraries.Converters
+{
+    public class BoolToTextDecorationStrikeThroughConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isCompleted = (bool)value;
+
+            return isCompleted ? TextDecorations.Strikethrough : TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

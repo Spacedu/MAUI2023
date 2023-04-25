4ce4a223bcf1f4c335e82bc033661da39922ebf4
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-25 10:45:30 -0300

    S15E02 - Animação - Scale

--- ProjetosMAUI/AppMAUIGallery/Views/Animations/BasicAnimation.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Animations/BasicAnimation.xaml.cs
@@ -6,4 +6,25 @@ public partial class BasicAnimation : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Normal(object sender, EventArgs e)
+    {
+        Image.Scale = 1;
+        Image.Opacity = 1;
+        Image.TranslationX = 0;
+        Image.TranslationY = 0;
+        Image.Rotation = 0;
+        Image.RotationX = 0;
+        Image.RotationY = 0;
+    }
+    private void Aumentar(object sender, EventArgs e)
+    {
+        Image.ScaleTo(2, 2000);
+    }
+
+    private void Diminuir(object sender, EventArgs e)
+    {
+        Image.ScaleTo(0.5, 2000);
+    }
+
 }

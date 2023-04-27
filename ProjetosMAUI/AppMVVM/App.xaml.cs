2fe2eac892f0492d262a04eea433697de0533a54
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-27 15:40:37 -0300

    S16E02 - Criando o projeto e estrutura inicial + View

---
+++ ProjetosMAUI/AppMVVM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppMVVM.Views;
+
+namespace AppMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+    }
+}
---
+++ ProjetosMAUI/AppMVVM/Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppMVVM.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}

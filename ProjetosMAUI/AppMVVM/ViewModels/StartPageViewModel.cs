7840a520bbe3c4a65ab7fee32a12c507567dc33f
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-28 09:47:52 -0300

    S16E05 - ViewModel - Command

--- ProjetosMAUI/AppMVVM/ViewModels/StartPageViewModel.cs
+++ ProjetosMAUI/AppMVVM/ViewModels/StartPageViewModel.cs
@@ -1,18 +1,50 @@
 ﻿using AppMVVM.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppMVVM.ViewModels
 {
-    public class StartPageViewModel
+    public class StartPageViewModel : INotifyPropertyChanged
     {
-        public Person Person { get; set; }
+        #region To Form.
+        private Person _person;
+        public Person Person
+        {
+            get { return _person; }
+            set { _person = value; OnPropertyChanged(nameof(Person)); }
+        }
+        public ICommand SaveCommand { get; set; }
+        #endregion
+
+        #region List
+        public ObservableCollection<Person> People { get; set; }
+        #endregion
+
         public StartPageViewModel()
         {
+            SaveCommand = new Command(Save);
             Person = new Person();
+            People = new ObservableCollection<Person>();
+        }
+
+        private void Save()
+        {
+            People.Add(Person);
+            Person = new Person();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }

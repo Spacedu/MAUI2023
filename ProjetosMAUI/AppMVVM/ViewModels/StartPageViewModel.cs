69a3060df9a795241891fa55ebc2163bd42fe706
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-28 08:54:58 -0300

    S16E04 - ViewModel - Binding e Notificação.trec

--- ProjetosMAUI/AppMVVM/ViewModels/StartPageViewModel.cs
+++ ProjetosMAUI/AppMVVM/ViewModels/StartPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace AppMVVM.ViewModels
 {
-    internal class StartPageViewModel
+    public class StartPageViewModel
     {
+        public Person Person { get; set; }
+        public StartPageViewModel()
+        {
+            Person = new Person();
+        }
     }
 }

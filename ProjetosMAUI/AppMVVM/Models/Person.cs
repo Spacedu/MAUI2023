a7d8c13a227e08836495b3ce3f3cf888eff04709
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-28 08:22:26 -0300

    S16E03 - Modelo - Person - INotifyPropertyChanged

--- ProjetosMAUI/AppMVVM/Models/Person.cs
+++ ProjetosMAUI/AppMVVM/Models/Person.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AppMVVM.Models
 {
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged(nameof(Id)); }
+        }
+        private string _name;
+        public string Name { 
+            get { return _name; } 
+            set { _name = value; OnPropertyChanged(nameof(Name)); } 
+        }
+
+        private string _email;
+        public string Email { 
+            get { return _email; }
+            set { _email = value; OnPropertyChanged(nameof(Email)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
     }
 }

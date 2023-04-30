4b223a009b4b6073122b0fff93d8a08b883e9393
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-29 23:08:33 -0300

    S18E07 - Usando Converters com Trigger

---
+++ ProjetosMAUI/AppMAUIGallery/Views/Utils/Converters/MoreThenZeroConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Views.Utils.Converters
+{
+    public class MoreThenZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueInt = (int)value;
+            return valueInt > 0 ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

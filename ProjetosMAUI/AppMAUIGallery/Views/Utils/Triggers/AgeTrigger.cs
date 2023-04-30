128c5b493f508f83998a7037bea36c317b136da4
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-04-29 21:44:02 -0300

    S18E05 - EventTrigger

---
+++ ProjetosMAUI/AppMAUIGallery/Views/Utils/Triggers/AgeTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Views.Utils.Triggers
+{
+    public class AgeTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int result;
+            bool isNumber = int.TryParse(sender.Text, out result);
+
+            if (isNumber)
+            {
+                if(result >= 18)
+                {
+                    sender.TextColor = Colors.Green;
+                }
+                else
+                {
+                    sender.TextColor = Colors.Red;
+                }
+            }
+            else
+            {
+                sender.TextColor = Colors.Purple;
+            }
+
+        }
+    }
+}

21fe59fb033fdd9bade6c16d87b21c63f54bdff8
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-02 22:45:02 -0300

    S19E07 - Behaviors - EventToCommand

--- ProjetosMAUI/AppMAUIGallery/Views/CommunityMaui/CommunityBehaviorPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/CommunityMaui/CommunityBehaviorPage.xaml.cs
@@ -1,3 +1,5 @@
+using AppMAUIGallery.Views.CommunityMaui.ViewModels;
+
 namespace AppMAUIGallery.Views.CommunityMaui;
 
 public partial class CommunityBehaviorPage : ContentPage
@@ -6,4 +8,12 @@ public partial class CommunityBehaviorPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnPressed(object sender, EventArgs e)
+    {
+		var vm = (CommunityBehaviorPageViewModel)BindingContext;
+
+		if(vm.PressedCommand.CanExecute(null))
+			vm.PressedCommand.Execute(null);
+    }
 }
---
+++ ProjetosMAUI/AppMAUIGallery/Views/CommunityMaui/ViewModels/CommunityBehaviorPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppMAUIGallery.Views.CommunityMaui.ViewModels
+{
+    public class CommunityBehaviorPageViewModel
+    {
+        public ICommand PressedCommand { get; set; }
+        public CommunityBehaviorPageViewModel()
+        {
+            PressedCommand = new Command(Pressed);
+        }
+
+        private void Pressed()
+        {
+            App.Current.MainPage.DisplayAlert("Fui pressionado!", "Fui pressionado!", "OK");
+        }
+    }
+}

1340e00ad8322b800e7ed061cd8d002f11ba7eba
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-09 16:47:35 -0300

    S20E10 - Shell - Navegação Rota Absoluta e Pesquisa

--- ProjetosMAUI/AppMAUIGallery/Views/Shells/Pages/Page01Step02.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Shells/Pages/Page01Step02.xaml.cs
@@ -6,4 +6,24 @@ public partial class Page01Step02 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void GoToRegisterStep02(object sender, EventArgs e)
+    {
+        // Rota atual: page01/page01step02
+
+        //Rotas: cadastro/pessoa/editar/passo3
+
+        //Relativas: cadastro/passo2
+        //Shell.Current.GoToAsync("../../cadastro/passo2");
+
+        //Rota Absoluta: //cadastro/passo2
+        Shell.Current.GoToAsync("//cadastro/passo2");
+
+
+    }
+
+    private void GoToRegisterStep01(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("///passo1");
+    }
 }

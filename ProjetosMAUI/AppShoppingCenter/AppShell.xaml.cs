5732630e929d654d6bc9df99215e590e39ba27da
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-15 07:50:01 -0300

    S22E02 - Criando o projeto e Instalado as Bibliotecas

---
+++ ProjetosMAUI/AppShoppingCenter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppShoppingCenter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProjetosMAUI/AppShoppingCenter/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace AppShoppingCenter
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMediaElement()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

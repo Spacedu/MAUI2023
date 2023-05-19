371c5b16d53ab67f51d8fb2cec982b7149cd29ad
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-19 18:57:12 -0300

    S22E10 - Stores - ListPageViewModel - Parte 1

---
+++ ProjetosMAUI/AppShoppingCenter/ViewModels/Stores/ListPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AppShoppingCenter.Models;
+using AppShoppingCenter.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppShoppingCenter.ViewModels.Stores
+{
+    public partial class ListPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string textSearch;
+
+        private List<Establishment> establishmentsFull;
+
+        [ObservableProperty]
+        private List<Establishment> establishmentsFiltered;
+
+        public ListPageViewModel()
+        {
+            var service = new StoreService();
+            establishmentsFull = service.GetStores();
+            establishmentsFiltered = establishmentsFull.ToList();
+        }
+    }
+}

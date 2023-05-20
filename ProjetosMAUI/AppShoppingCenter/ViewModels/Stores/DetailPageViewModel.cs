911a83d18d05d88bd3e940c596dd686d532e07d0
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-19 22:24:25 -0300

    S22E15 - Store - DetailPage - Recebendo dados da Rota

---
+++ ProjetosMAUI/AppShoppingCenter/ViewModels/Stores/DetailPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AppShoppingCenter.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppShoppingCenter.ViewModels.Stores
+{
+    [QueryProperty(nameof(Establishment), "establishment")]
+    public partial class DetailPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Establishment establishment;
+    }
+}

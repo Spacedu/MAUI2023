9159f2f09b6a7cc5409ace3430164267849291a6
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-25 19:35:20 -0300

    S22E33 - Cinema - Detail - Lógica Fechar e PlayPause - Parte 1

--- ProjetosMAUI/AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
+++ ProjetosMAUI/AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
@@ -2,8 +2,30 @@ namespace AppShoppingCenter.Views.Cinemas;
 
 public partial class DetailPage : ContentPage
 {
+	bool firstTime = false;
 	public DetailPage()
 	{
 		InitializeComponent();
 	}
+
+    private void PlayPause(object sender, TappedEventArgs e)
+    {
+		if(firstTime == false)
+		{
+			var mediaHalf = player.Width / 2;
+
+			btn_playpause.TranslateTo(-mediaHalf, mediaHalf, 500);
+			firstTime = true;
+		}
+		if(player.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+		{
+			player.Pause();
+			btn_playpause.Source = ImageSource.FromFile("play.png");
+		}
+		else
+		{
+			player.Play();
+            btn_playpause.Source = ImageSource.FromFile("pause.png");
+        }
+    }
 }

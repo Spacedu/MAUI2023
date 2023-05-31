1ffc935ca506fc910ea5c5b0fbd3f4ab94b73245
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-05-31 12:39:59 -0300

    S22E42 - Tickets - ScanPage - Navegação

--- ProjetosMAUI/AppShoppingCenter/ViewModels/Tickets/ScanPageViewModel.cs
+++ ProjetosMAUI/AppShoppingCenter/ViewModels/Tickets/ScanPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace AppShoppingCenter.ViewModels.Tickets
 {
-    class ScanPageViewModel
+    public partial class ScanPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string ticketNumber;
+
+        [RelayCommand]
+        private void Scan()
+        {
+            //TODO - Abrir a camera
+            Shell.Current.GoToAsync("pay");
+        }
+
+        [RelayCommand]
+        private void CheckTicketNumber()
+        {
+            if (TicketNumber?.Length < 12)
+                return;
+
+            Shell.Current.GoToAsync("pay");
+
+            //TODO - Verificar se código existe no banco/API.
+
+            //TODO - Navegar para página Pay.
+
+            //TODO - Mensagem de alerta.
+        }
+
+        [RelayCommand]
+        private void List()
+        {
+            Shell.Current.GoToAsync("list");
+        }
     }
 }

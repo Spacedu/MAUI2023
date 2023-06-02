3ea11015a304c2796a7a46708293bd98a0388242
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-06-02 14:35:05 -0300

    S22E48 - Ticket - PayPage - Data de Saida, Tolerancia e Valor

--- ProjetosMAUI/AppShoppingCenter/ViewModels/Tickets/PayPageViewModel.cs
+++ ProjetosMAUI/AppShoppingCenter/ViewModels/Tickets/PayPageViewModel.cs
@@ -11,7 +11,37 @@ namespace AppShoppingCenter.ViewModels.Tickets
     [QueryProperty(nameof(Ticket), "ticket")]
     public partial class PayPageViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private Ticket ticket; 
+        private Ticket ticket;
+
+        public Ticket Ticket
+        {
+            get { return ticket; }
+            set
+            {
+                GenerateDateOutAndTolerance(value);
+                GeneratePrice(value);
+                SetProperty(ref ticket, value);
+            }
+        }
+
+        private void GenerateDateOutAndTolerance(Ticket ticket)
+        {
+            var rd = new Random();
+            var hour = rd.Next(0, 12);
+            var minute = rd.Next(0, 60);
+
+            ticket.DateOut = ticket.DateIn.AddHours(hour).AddMinutes(minute);
+            ticket.DateTolerance = ticket.DateOut.AddMinutes(30);
+        }
+
+        private double HourValue = 0.10;
+
+        private void GeneratePrice(Ticket ticket)
+        {
+            var dif = ticket.DateOut.Ticks - ticket.DateIn.Ticks;
+            var difTS = new TimeSpan(dif);
+
+            ticket.Price = difTS.TotalMinutes * HourValue;
+        }
     }
 }

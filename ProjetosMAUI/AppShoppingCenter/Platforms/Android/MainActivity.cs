8ee0c987300543d2abb03b4294070da74b064124
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-06-03 19:28:34 -0300

    S22E61 - Keyboard Fix e Entry Margin

--- ProjetosMAUI/AppShoppingCenter/Platforms/Android/MainActivity.cs
+++ ProjetosMAUI/AppShoppingCenter/Platforms/Android/MainActivity.cs
@@ -1,11 +1,30 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views.InputMethods;
+using Android.Views;
+using Android.Widget;
 
 namespace AppShoppingCenter
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public override bool DispatchTouchEvent(MotionEvent e)
+        {
+            if (e.Action == MotionEventActions.Down)
+            {
+                var view = CurrentFocus;
+                if (view is EditText editText)
+                {
+                    editText.ClearFocus();
+                    InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+                    imm.HideSoftInputFromWindow(view.WindowToken, 0);
+                }
+            }
+
+            return base.DispatchTouchEvent(e);
+        }
     }
 }

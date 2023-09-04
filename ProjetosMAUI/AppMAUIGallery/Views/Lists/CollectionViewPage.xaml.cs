c19a0881fa5ca0e5512ac5461b5c891dae0fb1a5
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-09-03 21:27:13 -0300

    S11E14.1 - Correção do Scroll Infinito Windows

--- ProjetosMAUI/AppMAUIGallery/Views/Lists/CollectionViewPage.xaml.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/Lists/CollectionViewPage.xaml.cs
@@ -32,7 +32,7 @@ public partial class CollectionViewPage : ContentPage
 
 	private void AddTenMovies()
 	{
-		for(int i = 0; i < 20; i++)
+		for(int i = 0; i < 10; i++)
 		{
 			Movie movie = new Movie
 			{
@@ -46,4 +46,21 @@ public partial class CollectionViewPage : ContentPage
 		}
 
 	}
+
+    private void CollectionViewControl_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+		if (DeviceInfo.Platform != DevicePlatform.WinUI)
+			return;
+		
+		if(sender is CollectionView cv)
+		{
+			var LastVisibleItem = e.LastVisibleItemIndex; //17
+			var RemainingItemsThreshold = cv.RemainingItemsThreshold; //3
+			var TotalItem = ((IEnumerable<object>)cv.ItemsSource).Count(); //20 - 40 - 60...
+
+			if( LastVisibleItem > (TotalItem - RemainingItemsThreshold)){
+				AddTenMovies();
+			}
+		}
+    }
 }

66541befe7b0aec5631f054d0fa6a18af0b496b1
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-08 14:37:01 -0300

    S24E11 - UsersController - Configurando SMTP

---
+++ ProjetosMAUI/AppTask.API/Libraries/Emails/EmailAccessToken.cs
@@ -0,0 +1,20 @@
+﻿using AppTask.Models;
+using System.Net.Mail;
+
+namespace AppTask.API.Libraries.Emails
+{
+    public class EmailAccessToken
+    {
+        private SmtpClient _smtp;
+
+        public EmailAccessToken(SmtpClient smtp)
+        {
+            _smtp = smtp;
+        }
+
+        public void Send(UserModel userModel)
+        {
+
+        }
+    }
+}
--- ProjetosMAUI/AppTask.API/Program.cs
+++ ProjetosMAUI/AppTask.API/Program.cs
@@ -1,7 +1,13 @@
+using AppTask.API.Libraries.Emails;
 using AppTask.Database;
 using AppTask.Database.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Mail;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -12,6 +18,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppTaskContext>();
 builder.Services.AddScoped<IUserModelRepository, UserModelRepository>();
 builder.Services.AddScoped<ITaskModelRepository, TaskModelRepository>();
+builder.Services.AddScoped<SmtpClient>(options => {
+    var smtpClient = new SmtpClient(configuration.GetValue<string>("Mail:ServerSMTPAddress"), configuration.GetValue<int>("Mail:ServerSMTPPort"));
+    smtpClient.EnableSsl = configuration.GetValue<bool>("Mail:ServerSMTPEnableSSL");
+    smtpClient.Credentials = new NetworkCredential(
+        configuration.GetValue<string>("Mail:MailAddress"),
+        configuration.GetValue<string>("Mail:MailPassword")
+    );
+    return smtpClient;
+});
+builder.Services.AddScoped<EmailAccessToken>();
 
 var app = builder.Build();
 

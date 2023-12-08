9251f7b1f96587569c3044a678aa8040c60e6b43
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-08 11:13:22 -0300

    S24E06 - Criando UserRepository

---
+++ ProjetosMAUI/AppTask.API/Controllers/TasksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+    }
+}
---
+++ ProjetosMAUI/AppTask.Database/Repositories/IUserModelRepository.cs
@@ -0,0 +1,11 @@
+﻿using AppTask.Models;
+
+namespace AppTask.Database.Repositories
+{
+    public interface IUserModelRepository
+    {
+        void Add(UserModel user);
+        UserModel GetByEmail(string email);
+        void Update(UserModel user);
+    }
+}

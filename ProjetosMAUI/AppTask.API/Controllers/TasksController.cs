84dc526984688b00a23b03e56443ec5d8c601eef
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-08 15:35:43 -0300

    S24E15 - TasksController - Metodos CRUD

--- ProjetosMAUI/AppTask.API/Controllers/TasksController.cs
+++ ProjetosMAUI/AppTask.API/Controllers/TasksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppTask.Database.Repositories;
+using AppTask.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppTask.API.Controllers
@@ -7,5 +9,53 @@ namespace AppTask.API.Controllers
     [ApiController]
     public class TasksController : ControllerBase
     {
+        private ITaskModelRepository _repository;
+
+        public TasksController(ITaskModelRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //TODO - Implementar CRUD
+        [HttpGet]
+        public IActionResult GetAll(Guid userId)
+        {
+            var users = _repository.GetAll(userId);
+
+            if (users == null)
+                return NotFound();
+
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var task = _repository.GetById(id);
+            if (task == null)
+                return NotFound();
+
+            return Ok(task);
+        }
+        [HttpPost]
+        public IActionResult Add(TaskModel task)
+        {
+            _repository.Add(task);
+            return Ok(task);
+        }
+
+        [HttpPut]
+        public IActionResult Update(TaskModel task)
+        {
+            _repository.Update(task);
+            return Ok(task);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(TaskModel task)
+        {
+            _repository.Delete(task);
+            return Ok();
+        }
+
     }
 }

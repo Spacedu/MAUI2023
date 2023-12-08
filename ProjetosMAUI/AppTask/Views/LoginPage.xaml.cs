99a6e35ca71e6134128ee780fdd3fc01cbbb8b04
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-08 16:50:30 -0300

    S24E19 - AppTask - Login - Primeira Etapa do Login - Parte 2

--- ProjetosMAUI/AppTask/Views/LoginPage.xaml.cs
+++ ProjetosMAUI/AppTask/Views/LoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using AppTask.Libraries.Validations;
+
 namespace AppTask.Views;
 
 public partial class LoginPage : ContentPage
@@ -6,4 +8,20 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void NextAction(object sender, EventArgs e)
+    {
+		LblEmailValidateMessage.IsVisible = false;
+
+        if (!EmailValidate.IsValidEmail(EntryEmail.Text))
+		{
+			//TODO - Acionar API para Enviar AccessToken por e-mail.
+            LblEmailValidateMessage.IsVisible = true;
+			return;
+        }
+
+		EntryEmail.IsEnabled = false;
+		BtnNext.IsVisible = false;
+		Step2.IsVisible = true;
+    }
 }
